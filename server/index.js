66de161457b4ae3fa9c109c405c0fa68abe42aec
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-09 11:06:59 +0100

    updated the client side

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import corsOptions from "./config/allowedOrigins.js";
 
 import postRouters from "./routes/posts.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
