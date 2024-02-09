a20b013071296a9122baaf6af433d2cc7b3e0632
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-09 17:34:42 +0100

    doings

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,11 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
   try {
     const postMessage = await PostMessage.find();
     console.log(postMessage);
+    console.log("GET post");
     res.status(200).json(postMessage);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -17,15 +19,54 @@ export const createPost = async (req, res) => {
   try {
     await newPost.save();
     res.status(201).json(newPost);
+    console.log("POST");
   } catch (error) {
     res.status(409).json({ messahe: error.message });
   }
 };
 
-export const updatePost = (req, res) => {
-  res.send("Post Update");
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  console.log("UPDATE/");
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
 };
 
-export const deletePost = (req, res) => {
-  res.send("Delete Post ");
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  console.log("DELETE");
+
+  await PostMessage.findByIdAndDelete(id);
+
+  res.json({ message: `Deleted post ` });
+};
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  console.log("LIKED");
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+
+  res.json(updatedPost);
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,13 +4,15 @@ import {
   createPost,
   updatePost,
   deletePost,
+  likePost,
 } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
-router.put("/", updatePost);
-router.delete("/", deletePost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
 export default router;
