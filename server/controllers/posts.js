20b256135a6b844c5c7bc8c5debd3c5c69fb3397
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-17 00:29:23 +0100

    more

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,10 +6,23 @@ import PostMessage from "../models/postMessage.js";
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const postMessages = await PostMessage.find();
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // start index of posts to retrieve
+    const total = await PostMessage.countDocument({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
 
-    res.status(200).json(postMessages);
+    res
+      .status(200)
+      .json({
+        data: posts,
+        currentPage: Number(page),
+        numberOfPages: Math.ceil(total / LIMIT),
+      });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
