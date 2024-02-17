35e5a0f51c05a3a3637c8f3234609ef458084eef
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-17 01:54:55 +0100

    more

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,19 +10,17 @@ export const getPosts = async (req, res) => {
   try {
     const LIMIT = 8;
     const startIndex = (Number(page) - 1) * LIMIT; // start index of posts to retrieve
-    const total = await PostMessage.countDocument({});
+    const total = await PostMessage.countDocuments({});
     const posts = await PostMessage.find()
       .sort({ _id: -1 })
       .limit(LIMIT)
       .skip(startIndex);
 
-    res
-      .status(200)
-      .json({
-        data: posts,
-        currentPage: Number(page),
-        numberOfPages: Math.ceil(total / LIMIT),
-      });
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
