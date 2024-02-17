bfd9b91fa455ceb6ab18f89a309e623f178e4765
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-17 22:12:48 +0100

    doings

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,7 +28,6 @@ export const getPosts = async (req, res) => {
 
 export const getPost = async (req, res) => {
   const { id } = req.params;
-
   try {
     const post = await PostMessage.findById(id);
 
@@ -118,6 +117,22 @@ export const likePost = async (req, res) => {
     new: true,
   });
   res.status(200).json(updatedPost);
+};
+
+// comment logic
+export const commentPost = async (req, res) => {
+  const { id } = req.params;
+  const { value } = req.body;
+
+  const post = await PostMessage.findById(id);
+
+  post.comments.push(value);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };
 
 export default router;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,7 +2,9 @@ import express from "express";
 
 import {
   getPosts,
+  getPost,
   getPostBySearch,
+  commentPost,
   createPost,
   updatePost,
   likePost,
@@ -14,9 +16,11 @@ import auth from "../middleware/auth.js";
 
 router.get("/search", getPostBySearch);
 router.get("/", getPosts);
+router.get("/:id", getPost);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
 
 export default router;
